76ae83faa698b885043842cb0f7ead05e3f7562c
wasp <wasp@wasp>
2019-07-04 01:24:35 +0200

    network for containers added

--- webtentacle/splunk.py
+++ webtentacle/splunk.py
@@ -15,25 +15,39 @@ conf = settings.get_splunk()
 
 
 
-service = client.connect(host=socket.gethostname(),port=conf.get('port'),username='admin',password='changemeagain',cookie=0)
+service = client.connect(host=os.getenv('MOTHER_HOSTNAME'),
+                         port=conf.get('port'),
+                         username='admin',
+                         password=os.getenv('SPLUNK_INITIAL_PASSWORD'),
+                         cookie=0)
 
 def get_users():
     # get collection of users
-    return service.users
+    kwargs = {"sort_key":"username","sort_dir":"asc"}
+    return service.users.list(count=-1, **kwargs)
 
+
+users = get_users()
+print("Users:")
+for user in users:
+    print("{} {}".format(user.realname, user.name))
+    for role in user.role_entities:
+        print(" - ", role.name)
+        
 def create_user(username, password):
     user = service.users.create(username=username, password=password, roles=['power','user','admin'])
     return user
 
 
-#create_user('webtentacle','letmeinplease')
-#service.logout()
+usernames = map(lambda x: x.name, users)
+if os.getenv('SPLUNK_WEBTENTACLE_USER') not in usernames:
+    create_user(os.getenv('SPLUNK_WEBTENTACLE_USER'),os.getenv('SPLUNK_WEBTENTACLE_PASSWORD'))
+service.logout()
 
-#service = client.connect(host=socket.gethostname(),
-#                         port=conf.get('port'),
-#                         username='webtentacle',
-#                         password='letmeinplease',
-#                         cookie=0)
+service = client.connect(host='splunkenterprise',
+                         port=int(conf.get('port')),
+                         username=os.getenv('SPLUNK_WEBTENTACLE_USER'),
+                         password=os.getenv('SPLUNK_WEBTENTACLE_PASSWORD'))
 
 
 def echo_installed_apps():
